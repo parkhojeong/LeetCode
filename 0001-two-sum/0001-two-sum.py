e0c928904f291317655924cea934758ba020c599
parkhojeong <parkhj062@gmail.com>
2025-12-02 22:33:16 +0900

    Time: 4 ms (40.72%), Space: 19.8 MB (5.96%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,3 +1,15 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
+        dic = collections.defaultdict(int)
+        for index in range(len(nums)):
+            dic[nums[index]-target] = index
+
+        for index in range(len(nums)):
+            # print(index)
+            # if -1 * nums[index] not in dic:
+            #     continue
+
+            val = dic[-1 * nums[index]]
+            if val != 0 and index != val:
+                return [index, val]
         
