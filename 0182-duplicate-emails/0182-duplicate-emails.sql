27a09df804f535b93c627c39a7dd423ee143a4a8
parkhojeong <parkhj062@gmail.com>
2026-02-27 17:52:24 +0900

    Time: 370 ms (49.88%), Space: 0B (100%) - LeetHub

---
+++ 0182-duplicate-emails/0182-duplicate-emails.sql
@@ -0,0 +1,9 @@
+# Write your MySQL query statement below
+select distinct email as Email
+from Person
+where email in (
+    select email
+    from Person
+    group by email
+    having count(*) >= 2 
+)
