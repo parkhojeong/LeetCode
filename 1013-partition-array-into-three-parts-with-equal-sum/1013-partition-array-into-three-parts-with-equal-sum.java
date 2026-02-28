70332ea4435089cdcbe90a04463abdacea63b33e
parkhojeong <parkhj062@gmail.com>
2026-02-28 18:50:41 +0900

    Time: 1 ms (99.56%), Space: 55.4 MB (82.28%) - LeetHub

--- 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
+++ 1013-partition-array-into-three-parts-with-equal-sum/1013-partition-array-into-three-parts-with-equal-sum.java
@@ -1,27 +1,25 @@
 class Solution {
     public boolean canThreePartsEqualSum(int[] arr) {
-        for(int i = 1; i < arr.length-1; i++){
-            int sum1 = 0;
-            for(int a = 0; a < i; a++){
-                    sum1 += arr[a];
-            }
-            int sum2 = 0;
-            for(int a = i; a < i+1; a++){
-                sum2 += arr[a];
+        int sum = 0;
+        for(int i = 0; i < arr.length; i++){
+            sum += arr[i];
+        }
+
+        int sum1 = 0;
+        for(int i = 1; i < arr.length - 1; i++){
+            sum1 += arr[i-1];
+            int sum2 = arr[i];
+
+            if((sum1 * 3) != sum){
+                continue;
             }
 
-            int sum3 = 0;
-            for(int a = i+1; a < arr.length; a++){
-                sum3 += arr[a];
-            }
             for (int j = i + 1; j < arr.length; j++) {
-                // System.out.println(i + " " + j + " " + sum1 +" "+ sum2+" "+ sum3);
-                if(sum1 == sum2 && sum2 == sum3){
+                if((sum2 * 3) == sum){
                     return true;
                 }
 
                 sum2 += arr[j];
-                sum3 -= arr[j];
             }
         }
 
