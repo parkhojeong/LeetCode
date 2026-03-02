19b19058777600b261e1a483f46f2cd31d397db9
parkhojeong <parkhj062@gmail.com>
2026-03-02 22:12:16 +0900

    Time: 2 ms (98.14%), Space: 43.9 MB (48.3%) - LeetHub

--- 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
+++ 2591-distribute-money-to-maximum-children/2591-distribute-money-to-maximum-children.java
@@ -1,16 +1,16 @@
 class Solution {
     public int distMoney(int money, int children) {
-        if(money < children){ 
+        if(money < children){ // rule1,2 are not follwing 
             return -1;
         }
 
         int output = 0;
         while(children > 0){
-            if(money < (8 + (children - 1))){ // rule 1,2
+            if(money < (8 + (children - 1))){ // anyone can't get 8 dollar anymore
                 break;
             }
 
-            if(children == 1 && money > 8){ 
+            if(children == 1 && money > 8){ // the last children takes all money
                 break;
             }
 
@@ -22,7 +22,7 @@ class Solution {
             children -= 1;
         }
 
-        if(money == 4 && children == 1){ // rule 3
+        if(money == 4 && children == 1){ // for rule 3
             output -= 1;
         }
 
