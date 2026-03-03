3fa13b962f3230a4167659c5a8c5ed1506919c38
parkhojeong <parkhj062@gmail.com>
2026-03-03 20:32:39 +0900

    Time: 1 ms (100%), Space: 46.9 MB (84.03%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -1,33 +1,33 @@
 class Solution {
     public int earliestFinishTime(int[] landStartTime, int[] landDuration, int[] waterStartTime, int[] waterDuration) {
-        int finishTime1 = getFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
-        int finishTime2 = getFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
+        int finishTime1 = getEarliestFinishTime(landStartTime, landDuration, waterStartTime, waterDuration);
+        int finishTime2 = getEarliestFinishTime(waterStartTime, waterDuration, landStartTime, landDuration);
         return Math.min(finishTime1, finishTime2);
     }
 
-    public int getFinishTime(int[] aStartTime, int[] aDuration, int[] bStartTime, int[] bDuration) {
-        int aFinishTime = aStartTime[0] + aDuration[0];
+    public int getEarliestFinishTime(int[] aStartTime, int[] aDuration, int[] bStartTime, int[] bDuration) {
+        int aEarliestFinishTime = Integer.MAX_VALUE;
         for(int i = 0; i < aStartTime.length; i++){
-            if(aStartTime[i] + aDuration[i] < aFinishTime){
-                aFinishTime = aStartTime[i] + aDuration[i];
+            if(aStartTime[i] + aDuration[i] < aEarliestFinishTime){
+                aEarliestFinishTime = aStartTime[i] + aDuration[i];
             }
         }
 
-        int finishTime = Integer.MAX_VALUE;
+        int earliestFinishTime = Integer.MAX_VALUE;
         for(int i = 0; i < bStartTime.length; i++){
             int duration = bDuration[i];
-            int newFinishTime = 0;
-            if(bStartTime[i] <= aFinishTime){
-                newFinishTime = aFinishTime + duration;
+            int finishTime = 0;
+            if(bStartTime[i] <= aEarliestFinishTime){
+                finishTime = aEarliestFinishTime + duration;
             } else {
-                newFinishTime = bStartTime[i] + duration;
+                finishTime = bStartTime[i] + duration;
             }
 
-            if(newFinishTime < finishTime){
-                finishTime = newFinishTime;
+            if(finishTime < earliestFinishTime){
+                earliestFinishTime = finishTime;
             }
         }
 
-        return finishTime;
+        return earliestFinishTime;
     }
 }
