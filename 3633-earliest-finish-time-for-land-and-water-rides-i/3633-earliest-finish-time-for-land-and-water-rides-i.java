d30248a64a5453a93947e1218adabb80dff8dc87
parkhojeong <parkhj062@gmail.com>
2026-03-03 20:29:26 +0900

    Time: 1 ms (100%), Space: 46.9 MB (63.89%) - LeetHub

--- 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
+++ 3633-earliest-finish-time-for-land-and-water-rides-i/3633-earliest-finish-time-for-land-and-water-rides-i.java
@@ -6,27 +6,28 @@ class Solution {
     }
 
     public int getFinishTime(int[] aStartTime, int[] aDuration, int[] bStartTime, int[] bDuration) {
-        int aEndTime = aStartTime[0] + aDuration[0];
+        int aFinishTime = aStartTime[0] + aDuration[0];
         for(int i = 0; i < aStartTime.length; i++){
-            if(aStartTime[i] + aDuration[i] < aEndTime){
-                aEndTime = aStartTime[i] + aDuration[i];
+            if(aStartTime[i] + aDuration[i] < aFinishTime){
+                aFinishTime = aStartTime[i] + aDuration[i];
             }
         }
 
-        int output = Integer.MAX_VALUE;
+        int finishTime = Integer.MAX_VALUE;
         for(int i = 0; i < bStartTime.length; i++){
             int duration = bDuration[i];
-            if(bStartTime[i] <= aEndTime){
-                if(aEndTime + duration < output){
-                    output = aEndTime + duration;
-                }
+            int newFinishTime = 0;
+            if(bStartTime[i] <= aFinishTime){
+                newFinishTime = aFinishTime + duration;
             } else {
-                if(bStartTime[i] + duration < output) {
-                    output = bStartTime[i] + duration;
-                }
+                newFinishTime = bStartTime[i] + duration;
+            }
+
+            if(newFinishTime < finishTime){
+                finishTime = newFinishTime;
             }
         }
 
-        return output;
+        return finishTime;
     }
 }
