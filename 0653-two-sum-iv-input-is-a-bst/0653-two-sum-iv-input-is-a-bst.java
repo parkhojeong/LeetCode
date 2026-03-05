260d0715b16dfa81b21d128934047886a92696c3
parkhojeong <parkhj062@gmail.com>
2026-03-05 19:43:47 +0900

    Time: 7 ms (5.71%), Space: 47.4 MB (52.89%) - LeetHub

--- 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
+++ 0653-two-sum-iv-input-is-a-bst/0653-two-sum-iv-input-is-a-bst.java
@@ -16,16 +16,45 @@
 class Solution {
     Map<Integer, Integer> valToCountMap = new HashMap<>();
     public boolean findTarget(TreeNode root, int k) {
-        dfs(root);
+        Stack<TreeNode> stack = new Stack<>();
+        stack.add(root);
 
-        for(int val: valToCountMap.keySet()){
-            if(k == val * 2) {
-                if(valToCountMap.getOrDefault(k, 0) >= 2) return true;
+        Map<Integer, Integer> map = new HashMap<>();
+
+        while(!stack.isEmpty()){
+            TreeNode node = stack.pop();
+            if(node.left == null && node.right == null){
+                if(map.getOrDefault(k - node.val, 0) - 1 >= 0){
+                    return true;
+                }
+
+                map.put(node.val, map.getOrDefault(node.val, 0) + 1);
+                // do logic
+                // System.out.println(node.val);
+                continue;
             }
-            else if(valToCountMap.getOrDefault(k - val, 0) >= 1){
-                return true;
-            }
+
+            TreeNode left = node.left;
+            TreeNode right = node.right;
+            node.left = null;
+            node.right = null;
+
+            if(right != null) stack.push(right);
+            stack.push(node);
+            if(left != null) stack.push(left);
         }
+
+        System.out.println(stack);
+        // dfs(root);
+
+        // for(int val: valToCountMap.keySet()){
+        //     if(k == val * 2) {
+        //         if(valToCountMap.getOrDefault(k, 0) >= 2) return true;
+        //     }
+        //     else if(valToCountMap.getOrDefault(k - val, 0) >= 1){
+        //         return true;
+        //     }
+        // }
         
         return false;
     }
