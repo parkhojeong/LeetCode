bb7f7bce28566aba774be9ada156775b326a152b
parkhojeong <parkhj062@gmail.com>
2026-03-05 22:20:41 +0900

    Time: 401 ms (72.47%), Space: 0B (100%) - LeetHub

--- 0197-rising-temperature/0197-rising-temperature.sql
+++ 0197-rising-temperature/0197-rising-temperature.sql
@@ -1,8 +1,10 @@
 # Write your MySQL query statement below
-select id
+select w1.id
 from Weather w1
-where temperature > (
-    select temperature
-    from Weather w2
-    where w2.recordDate = SUBDATE(w1.recordDate, 1)
-)
+join Weather w2 on SUBDATE(w1.recordDate, 1) = w2.recordDate
+where w1.temperature > w2.temperature
+-- where temperature > (
+--     select temperature
+--     from Weather w2
+--     where w2.recordDate = SUBDATE(w1.recordDate, 1)
+-- )
