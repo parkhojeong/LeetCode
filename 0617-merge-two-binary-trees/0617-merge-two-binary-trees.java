feda41706d60e0ca890ad2cff407d0c02d63e293
parkhojeong <parkhj062@gmail.com>
2026-03-05 14:01:27 +0900

    Time: 1 ms (14.9%), Space: 47.2 MB (33.75%) - LeetHub

---
+++ 0617-merge-two-binary-trees/0617-merge-two-binary-trees.java
@@ -0,0 +1,87 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode mergeTrees(TreeNode root1, TreeNode root2) {
+        if(root1 == null && root2 == null){
+            return null;
+        }
+        TreeNode node = new TreeNode();
+        dfs(node, root1, root2);
+        return node;
+    }
+
+    void dfs(TreeNode node, TreeNode node1, TreeNode node2) {
+        if(node1 == null && node2 != null) {
+            node.val = node2.val;
+        } else if(node1 != null && node2 == null) {
+            node.val = node1.val;
+        } else {
+            node.val = node1.val + node2.val;
+        }
+
+        if(node1 != null && node1.left != null) {
+            TreeNode left = new TreeNode(node1.left.val);
+            node.left = left;
+
+            if(node2 != null && node2.left != null) {
+                left.val += node2.val;
+                dfs(left, node1.left, node2.left);
+            } else {
+                dfs(left, node1.left, null);
+            }
+            
+        } else if(node2 != null && node2.left != null) {
+            TreeNode left = new TreeNode(node2.left.val);
+            node.left = left;
+
+            if(node1 != null && node1.left != null) {
+                left.val += node1.val;
+                dfs(left, node1.left, node2.left);
+            }else{
+                dfs(left, null, node2.left);
+            }
+        }
+
+        if(node1 != null && node1.right != null) {
+            TreeNode right = new TreeNode(node1.right.val);
+            node.right = right;
+
+            if(node2 != null && node2.right != null) {
+                right.val += node2.val;    
+                dfs(right, node1.right, node2.right);
+            } else {
+                dfs(right, node1.right, null);
+            }
+        
+            
+        } else if(node2 != null && node2.right != null) {
+            TreeNode right = new TreeNode(node2.right.val);
+            node.right = right;
+
+            if(node1 != null && node1.right != null) {
+                right.val += node1.val;
+                dfs(right, node1.right, node2.right);
+            } else {
+                dfs(right, null, node2.right);
+            }
+
+        }
+
+
+    }
+
+    
+}
