7c3dc6dc126b714ab744156a9ddad990ddd53909
parkhojeong <parkhj062@gmail.com>
2026-03-07 18:12:49 +0900

    Time: 0 ms (100%), Space: 42.9 MB (51.02%) - LeetHub

--- 0100-same-tree/0100-same-tree.java
+++ 0100-same-tree/0100-same-tree.java
@@ -15,7 +15,47 @@
  */
 class Solution {
     public boolean isSameTree(TreeNode p, TreeNode q) {
-        return dfs(p, q);
+        return bfs(p, q);
+        // return dfs(p, q);
+    }
+
+    public boolean bfs(TreeNode p, TreeNode q) {
+        if(p == null && q == null) return true;
+        if((p != null && q == null) || (p == null && q != null)) return false;
+
+        Queue<TreeNode> q1 = new ArrayDeque<>();
+        Queue<TreeNode> q2 = new ArrayDeque<>();
+        q1.add(p);
+        q2.add(q);
+
+        while(!q1.isEmpty()) {
+            TreeNode n1 = q1.poll();
+            TreeNode n2 = q2.poll();
+
+            if(n1.val != n2.val){
+                return false;
+            }
+
+            if((n1.left == null && n2.left != null)
+            || (n1.left != null && n2.left == null)) return false;
+
+            if((n1.right == null && n2.right != null)
+            || (n1.right != null && n2.right == null)) return false;
+
+            if(n1.left != null) {
+                q1.add(n1.left);
+                q2.add(n2.left);
+            } 
+
+            if(n1.right != null) {
+                q1.add(n1.right);
+                q2.add(n2.right);
+            } 
+        }
+
+        if(!q2.isEmpty()) return false;
+
+        return true;
     }
 
     public boolean dfs(TreeNode p, TreeNode q) {
