2de4cd350f8f0037dce7773f46585b0a90e2e2da
parkhojeong <parkhj062@gmail.com>
2026-03-07 18:47:31 +0900

    Time: 140 ms (0.56%), Space: 222.1 MB (14.39%) - LeetHub

--- 0101-symmetric-tree/0101-symmetric-tree.java
+++ 0101-symmetric-tree/0101-symmetric-tree.java
@@ -15,8 +15,50 @@
  */
 class Solution {
     public boolean isSymmetric(TreeNode root) {
+        return bfs(root);
+        // return dfs(root.left, root.right);
+    }
 
-        return dfs(root.left, root.right);
+    boolean bfs(TreeNode node) {
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(node);
+
+        while(!q.isEmpty()) {
+            List<TreeNode> list = new ArrayList<>();
+            while(!q.isEmpty()){
+                list.add(q.poll());
+            }
+
+            int left = 0;
+            int right = list.size() - 1;
+            while(left < right){
+                TreeNode n1 = list.get(left);
+                TreeNode n2 = list.get(right);
+
+                if(n1.val != n2.val) return false;
+
+                left++;
+                right--;
+            }
+
+            boolean areAllLeaves = true;
+            for(int i = 0; i < list.size(); i++){
+                TreeNode n = list.get(i);
+                if(n.left != null || n.right != null) areAllLeaves = false;
+            }
+            if(areAllLeaves) break;
+
+            for(int i = 0; i < list.size(); i++){
+                TreeNode n = list.get(i);
+                if(n.left == null) q.add(new TreeNode(-1000));
+                else q.add(n.left);
+
+                if(n.right == null) q.add(new TreeNode(-1000));
+                else q.add(n.right);    
+            }
+        }
+
+        return true;
     }
 
     private boolean dfs(TreeNode root1, TreeNode root2){
