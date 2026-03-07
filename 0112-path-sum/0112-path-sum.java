89c773a87fd3480f82b6d92eb73bb8f03fdbc628
parkhojeong <parkhj062@gmail.com>
2026-03-07 20:56:13 +0900

    Time: 1 ms (6.06%), Space: 45.3 MB (12.83%) - LeetHub

--- 0112-path-sum/0112-path-sum.java
+++ 0112-path-sum/0112-path-sum.java
@@ -15,7 +15,38 @@
  */
 class Solution {
     public boolean hasPathSum(TreeNode root, int targetSum) {
-        return dfs(root, 0, targetSum);
+        return bfs(root, targetSum);
+        // return dfs(root, 0, targetSum);
+    }
+
+    boolean bfs(TreeNode node, int targetSum) {
+        if(node == null) return false;
+
+        Queue<TreeNode> q = new ArrayDeque<>();
+        q.add(node);
+
+        while(!q.isEmpty()){
+            List<TreeNode> list = new ArrayList<>();
+            while(!q.isEmpty()){
+                list.add(q.poll());
+            }
+
+            for(int i = 0; i < list.size(); i++){
+                TreeNode n = list.get(i);
+                if(n.left == null && n.right == null && n.val == targetSum) return true;
+
+                if(n.left != null){
+                    n.left.val += n.val;
+                    q.add(n.left);
+                }
+                if(n.right != null){
+                    n.right.val += n.val;
+                    q.add(n.right);
+                }
+            }
+        }
+
+        return false;
     }
 
     private boolean dfs(TreeNode node, int sum, int targetSum) {
