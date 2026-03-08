a40b2a1058fd8fc7dce13296eb1815036b91fbc0
parkhojeong <parkhj062@gmail.com>
2026-03-08 18:40:18 +0900

    Time: 0 ms (100%), Space: 42 MB (86.34%) - LeetHub

---
+++ 1025-divisor-game/1025-divisor-game.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean divisorGame(int n) {
+        return n % 2 == 0;
+    }
+}
+// 1 - false
+// 2 - true
+// 3 - false
+// 4 - true
+// 5 - false
+// 6 - true
+// 7 - false
+// 8 - true
+// 9 - false
+// 10 - true
+// 11 - false
+// 12 - true
+// 13 - false
+// 14 - true
+// 15 - false
+// 16 - true
+// 17 - false
+// 18 - true
+// 19 - 
