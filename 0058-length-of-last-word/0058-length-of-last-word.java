38359f8432578b5c1a7029502b80ea58206f7448
parkhojeong <parkhj062@gmail.com>
2026-03-11 08:51:50 +0000

    Time: 0 ms (100%), Space: 42.7 MB (98.11%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -1,6 +1,20 @@
 class Solution {
     public int lengthOfLastWord(String s) {
-        String[] arr = s.trim().split("\\s+");
-        return arr[arr.length-1].length();
+        int i = s.length() - 1;
+        int count = 0;
+        boolean isStarted = false;
+        while(i >= 0){
+            if(isStarted && s.charAt(i) == ' ') break;
+
+            if(s.charAt(i) == ' ') i--;
+            else {
+                isStarted = true;
+                count += 1;
+                i--;
+            }
+        }
+        return count; 
+        // String[] arr = s.trim().split("\\s+");
+        // return arr[arr.length-1].length();
     }
 }
