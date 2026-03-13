064c8428992e23922429476777cf128c10d69685
parkhojeong <parkhj062@gmail.com>
2026-03-13 10:41:40 +0000

    Time: 3 ms (30.9%), Space: 119.5 MB (61.41%) - LeetHub

--- 0134-gas-station/0134-gas-station.java
+++ 0134-gas-station/0134-gas-station.java
@@ -3,44 +3,37 @@ class Solution {
         int gasSum = 0;
         int costSum = 0;
         int len = gas.length;
-        int[] arr = new int[len];
-        for(int i = 0; i < gas.length; i++) {
+
+        int[] arr = new int[len];   
+        for(int i = 0; i < len; i++) {
             gasSum += gas[i];
             costSum += cost[i];
             arr[i] = gas[i] - cost[i];
         }
-        // System.out.println(Arrays.toString(gas));
-        // System.out.println(Arrays.toString(cost));
-        // System.out.println(Arrays.toString(arr));
 
         if(costSum > gasSum) return -1;
 
         int findStartIndex = -1;
-        for(int i = 0; i < gas.length; i++) {
-            int diff1 = gas[i] - cost[i];
+        for(int i = 0; i < len; i++) {
             int j = (i+1) % len;
-            int diff2 = gas[j] - cost[j];
-            if(diff1 < 0 && diff2 > 0){
+            if(arr[i] < 0 && arr[j] > 0){
                 findStartIndex = i;
                 break;
             }
         }
 
-        // System.out.println(findStartIndex);
-
         int i = 0;
         int sum = 0; 
         
-        while(i < gas.length) {
+        while(i < len) {
             int cur = getPositiveIndex(findStartIndex - i, len);
             sum += arr[cur];
             if(sum < 0) i++;
             else {
-                while(i < gas.length) {
+                while(i < len) {
                     cur = getPositiveIndex(findStartIndex - i, len);
-                    if(arr[cur] >= 0){
-                        ;
-                    } else {
+                    
+                    if(arr[cur] < 0){
                         i--;
                         return getPositiveIndex(findStartIndex - i, len);
                     }
@@ -50,7 +43,6 @@ class Solution {
             }
         }
 
-        // System.out.println(findStartIndex +", " + i);
         return getPositiveIndex(findStartIndex - i + 1 , len);
     }
 
