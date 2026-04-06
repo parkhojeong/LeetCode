d7be2d5c2f799b6d4843c99459d7c350721ba445
hojeong park <parkhj062@gmail.com>
2026-04-07 02:18:07 +0900

    Time: 2 ms (43.13%), Space: 19.2 MB (63.63%) - LeetHub

--- 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
+++ 1716-calculate-money-in-leetcode-bank/1716-calculate-money-in-leetcode-bank.py
@@ -1,4 +1,4 @@
 class Solution:
     def totalMoney(self, n: int) -> int:
-        li = [(((x - 1) % 7 + 1) + (x - 1) // 7) for x in range(1, n + 1)]
+        li = [((x % 7 + 1) + x // 7) for x in range(n)]
         return sum(li)
